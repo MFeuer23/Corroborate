c90f2bc83fd40016609fc08b66dc356ed8a3e3f8
Mallory Feuer <mallory.feuer@gmail.com>
2018-05-08 15:36:48 +0000

    added subjects table migration, edited claims migration

--- db/migrate/20180508152712_create_claims.rb
+++ db/migrate/20180508152712_create_claims.rb
@@ -3,6 +3,7 @@ class CreateClaims < ActiveRecord::Migration[5.2]
     create_table :claims do |t|
       t.timestamps
       t.string :content
+      t.string :location
       t.string :contact
       t.integer :user_id
       t.integer :subject_id
