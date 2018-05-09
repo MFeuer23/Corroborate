0a8aeab2475c3a1fea9a27aaf1a26b679abd55a1
Mallory Feuer <mallory.feuer@gmail.com>
2018-05-09 14:35:54 +0000

    stubbed out basic user controller routes

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,3 +1,15 @@
 class UserController < ApplicationController
   
+  get "/user/signup" do
+    erb :"user/signup"
+  end
+  
+  get "/user/login" do
+    erb :"user/login"
+  end
+  
+  get "/user/:id" do
+    @user = User.find(params[:id])
+    erb :"user/index"
+  end
 end
