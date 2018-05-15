750b50742dde2df6eb7fe04c1d476eaee2db4ac6
Mallory Feuer <mallory.feuer@gmail.com>
2018-05-15 14:59:51 +0000

    added routes to subject_controller.rb

--- app/controllers/subject_controller.rb
+++ app/controllers/subject_controller.rb
@@ -1,3 +1,13 @@
 class SubjectController < ApplicationController
 
+  get "/subjects/search" do
+    erb :"/subjects/search_subjects"
+  end
+  
+  
+  
+  get "/subjects/:slug" do
+    @subject = Subject.find_by_slug(params[:slug])
+    erb :"/subjects/show"
+  end
 end
