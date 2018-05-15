e000347452da5f4e39294f4c736b143231b479fe
Mallory Feuer <mallory.feuer@gmail.com>
2018-05-15 14:57:04 +0000

    added logic to claims_controller.rb to handle new claims

--- app/controllers/claim_controller.rb
+++ app/controllers/claim_controller.rb
@@ -10,12 +10,23 @@ class ClaimController < ApplicationController
 
   post "/claims" do
     if logged_in?
-      @user = current_user
-      @subject = Subject.find_or_create_by_name(params[:subject])
-      @claim = Claim.create(params[:claim])
-      @subject.claims << @claim
-      @subject.save
-      redirect "/subjects/#{@subject.slug}"
+      if params[:agree] == "yes" && params[:subject][:name] != "" && params[:claim][:location] != "" && params[:claim][:content] !="" && params[:user][:content] != ""
+        @user = current_user
+        @subject = Subject.find_or_create_by_name(params[:subject])
+        @claim = Claim.create(params[:claim])
+        @subject.claims << @claim
+        @subject.save
+        if params[:user][:contact] == "yes"
+          @claim.contact = @user.email
+          @claim.save
+        else
+          @claim.contact = "no contact provided"
+          @claim.save
+        end
+        redirect "/subjects/#{@subject.slug}"
+      else
+        redirect "/claims/new_claim"
+      end
     else
       redirect "/"
     end
